5b8f465a228a984066a783f9369a3d30f778d658
Yaniv Israel <isyaniv@gmail.com>
2013-10-18 05:55:13 +0300

    recording added

---
+++ recordsln/JarvisServer/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace JarvisServer
+{
+    public static class JSONHelper
+    {
+        public static string ToJSON(this object obj)
+        {
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            return serializer.Serialize(obj);
+        }
+
+        public static string ToJSON(this object obj, int recursionDepth)
+        {
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            serializer.RecursionLimit = recursionDepth;
+            return serializer.Serialize(obj);
+        }
+        public static Reco FromJSON(string obj)
+        {
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            Reco obj1 = serializer.Deserialize<Reco>(obj);
+            return obj1;
+        }
+
+    }
+
+    public class Reco
+    {
+        public string status { get; set; }
+        public string id { get; set; }
+
+        public System.Collections.ArrayList hypotheses { get; set; }
+    }
+}
